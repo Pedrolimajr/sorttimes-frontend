314fd04bc43ec945ad9622e2aa0c6786ab83337a
Pedrolimajr <pedrosouzalima@hotmail.com>
2025-05-29 20:32:13 -0300

    Resolução de problemas de proteção da rotas

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,55 +1,31 @@
 // src/routes/AppRoutes.jsx
-// src/routes/AppRoutes.jsx
-import React, { useEffect } from 'react';
-import { Routes, Route, useLocation, useNavigate, Navigate } from "react-router-dom"; // Adicionei Navigate aqui
-import Home from "../pages/Home";
-import Login from "../pages/Login";
-import Dashboard from "../pages/Dashboard";
-import CadastroJogadores from "../pages/CadastroJogadores";
-import AgendarPartida from "../pages/AgendarPartida";
-import PartidasAgendadas from "../pages/PartidasAgendadas";
-import SorteioTimes from "../pages/SorteioTimes";
-import Financeiro from "../pages/Financeiro";
-import ListaJogadores from "../pages/ListaJogadores";
-import RecuperarSenha from "../pages/RecuperarSenha";
-import Cadastro from "../pages/Cadastro";
-import InformacoesPartida from "../pages/InformacoesPartida";
-import ConfiguracoesConta from "../pages/ConfiguracoesConta";
+import React from 'react';
+import { Routes, Route, Navigate, useLocation } from 'react-router-dom';
+import Home from '../pages/Home';
+import Login from '../pages/Login';
+import Dashboard from '../pages/Dashboard';
+import CadastroJogadores from '../pages/CadastroJogadores';
+import AgendarPartida from '../pages/AgendarPartida';
+import PartidasAgendadas from '../pages/PartidasAgendadas';
+import SorteioTimes from '../pages/SorteioTimes';
+import Financeiro from '../pages/Financeiro';
+import ListaJogadores from '../pages/ListaJogadores';
+import RecuperarSenha from '../pages/RecuperarSenha';
+import Cadastro from '../pages/Cadastro';
+import InformacoesPartida from '../pages/InformacoesPartida';
+import ConfiguracoesConta from '../pages/ConfiguracoesConta';
 import ConfirmarPresenca from '../pages/ConfirmarPresenca';
-import PrivateRoute from './PrivateRoute';
 import { authService } from '../services/authService';
+
+// Componente PrivateRoute simplificado e eficaz
+const PrivateRoute = ({ children }) => {
+  const location = useLocation();
+  return authService.isAuthenticated() ? children : <Navigate to="/login" state={{ from: location }} replace />;
+};
 
 function AppRoutes() {
   const location = useLocation();
-  const navigate = useNavigate();
   const isFixedHeight = ['/', '/login', '/cadastro', '/recuperar-senha'].includes(location.pathname);
-
-  // Verificação global de autenticação
-  useEffect(() => {
-    const publicRoutes = [
-      '/',
-      '/login', 
-      '/cadastro',
-      '/recuperar-senha',
-      /^\/confirmar-presenca\/\w+$/ // Regex para rotas com parâmetros
-    ];
-
-    const isPublicRoute = publicRoutes.some(route => {
-      if (typeof route === 'string') {
-        return route === location.pathname;
-      } else if (route instanceof RegExp) {
-        return route.test(location.pathname);
-      }
-      return false;
-    });
-
-    if (!isPublicRoute && !authService.isAuthenticated()) {
-      navigate('/login', { 
-        state: { from: location.pathname },
-        replace: true 
-      });
-    }
-  }, [location, navigate]);
 
   return (
     <div className={isFixedHeight ? 'h-[calc(100vh-64px)]' : 'min-h-full'}>
@@ -57,23 +33,24 @@ function AppRoutes() {
         {/* Rotas públicas */}
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/recuperar-senha" element={<RecuperarSenha />} />
         <Route path="/cadastro" element={<Cadastro />} />
+        <Route path="/recuperar-senha" element={<RecuperarSenha />} />
         <Route path="/confirmar-presenca/:linkId" element={<ConfirmarPresenca />} />
 
         {/* Rotas protegidas */}
-        <Route path="/dashboard" element={
-          <PrivateRoute>
-            <Dashboard />
-          </PrivateRoute>
-        } />
-        
-        {/* ... outras rotas protegidas ... */}
-        
-        {/* Rota de fallback - redireciona para login */}
-        <Route path="*" element={
-          <Navigate to="/login" replace />
-        } />
+        <Route path="/dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
+        <Route path="/cadastro-jogadores" element={<PrivateRoute><CadastroJogadores /></PrivateRoute>} />
+        <Route path="/lista-jogadores" element={<PrivateRoute><ListaJogadores /></PrivateRoute>} />
+        <Route path="/agendar-partida" element={<PrivateRoute><AgendarPartida /></PrivateRoute>} />
+        <Route path="/partidas-agendadas" element={<PrivateRoute><PartidasAgendadas /></PrivateRoute>} />
+        <Route path="/informacoes-partida/:id" element={<PrivateRoute><InformacoesPartida /></PrivateRoute>} />
+        <Route path="/informacoes-partida" element={<PrivateRoute><InformacoesPartida /></PrivateRoute>} />
+        <Route path="/sorteio-times" element={<PrivateRoute><SorteioTimes /></PrivateRoute>} />
+        <Route path="/financeiro" element={<PrivateRoute><Financeiro /></PrivateRoute>} />
+        <Route path="/configuracoes" element={<PrivateRoute><ConfiguracoesConta /></PrivateRoute>} />
+
+        {/* Rota de fallback */}
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </div>
   );
--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,9 +1,19 @@
 import { Navigate, useLocation } from 'react-router-dom';
 import { authService } from '../services/authService';
+import { useEffect } from 'react';
+import { toast } from 'react-toastify';
 
 const PrivateRoute = ({ children }) => {
   const location = useLocation();
-  return authService.isAuthenticated() ? children : <Navigate to="/login" state={{ from: location }} replace />;
+  const isAuth = authService.isAuthenticated();
+
+  useEffect(() => {
+    if (!isAuth) {
+      toast.error('Acesso restrito - Faça login para continuar');
+    }
+  }, [isAuth]);
+
+  return isAuth ? children : <Navigate to="/login" state={{ from: location }} replace />;
 };
 
 export default PrivateRoute;
