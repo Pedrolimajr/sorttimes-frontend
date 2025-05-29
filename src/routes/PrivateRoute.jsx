6656d996347bd31ba3c89f7f0bf8d86fa1ff82e9
Pedrolimajr <pedrosouzalima@hotmail.com>
2025-05-29 14:15:26 -0300

    Ajustes finais

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -4,11 +4,13 @@ import { authService } from '../services/authService';
 
 const PrivateRoute = ({ children }) => {
   const isAuth = authService.isAuthenticated();
-
-  console.log("🔒 Acesso verificado. Usuário autenticado?", isAuth);
+  console.log("🔒 Verificando autenticação:", isAuth);
+  console.log("🔑 Token no localStorage:", localStorage.getItem('token'));
+  console.log("👤 User no localStorage:", localStorage.getItem('user'));
 
   return isAuth ? children : <Navigate to="/login" replace />;
 };
 
 export default PrivateRoute;
+
 
