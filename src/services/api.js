3b62e673a7792da530d344f74f4ee3841f6656eb
Pedrolimajr <pedrosouzalima@hotmail.com>
2025-06-11 12:15:07 -0300

    Ajustando api.js

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { toast } from 'react-toastify';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: process.env.REACT_APP_API_URL || 'https://sorttimes-backend.onrender.com/api',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json'
