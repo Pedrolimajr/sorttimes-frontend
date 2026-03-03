85c3a489bef61e60917c427ba73d885f119874b4
Pedrolimajr <pedrosouzalima@hotmail.com>
2026-03-03 19:48:47 -0300

    Reajustando

--- src/pages/ConfirmarPresenca.jsx
+++ src/pages/ConfirmarPresenca.jsx
@@ -1,3 +1,5 @@
+
+
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { motion, AnimatePresence } from 'framer-motion';
@@ -225,3 +227,5 @@ export default function ConfirmarPresenca() {
     </div>
   );
 }
+
+
